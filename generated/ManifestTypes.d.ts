8036c5fe2cbe357635b1cf4f8811d58e8e7d525c
MY Dynamics <moly_omidvar@hotmail.es>
2021-05-28 09:24:42 +0200

    update

--- generated/ManifestTypes.d.ts
+++ generated/ManifestTypes.d.ts
@@ -5,7 +5,6 @@
 // Define IInputs and IOutputs Type. They should match with ControlManifest.
 export interface IInputs {
     value: ComponentFramework.PropertyTypes.StringProperty;
-<<<<<<< HEAD
     tagEntityName: ComponentFramework.PropertyTypes.StringProperty;
     tagEntityNameAttribute: ComponentFramework.PropertyTypes.StringProperty;
     tagEntityIdAttribute: ComponentFramework.PropertyTypes.StringProperty;
@@ -16,15 +15,6 @@ export interface IInputs {
     connectionRecord1Field: ComponentFramework.PropertyTypes.StringProperty;
     connectionRecord2Field: ComponentFramework.PropertyTypes.StringProperty;
     FetchXmlTagResource: ComponentFramework.PropertyTypes.StringProperty;
-=======
-    linkedEntityName: ComponentFramework.PropertyTypes.StringProperty;
-    nameAttribute: ComponentFramework.PropertyTypes.StringProperty;
-    idAttribute: ComponentFramework.PropertyTypes.StringProperty;
-    otherNameAttribute: ComponentFramework.PropertyTypes.StringProperty;
-    relationshipName: ComponentFramework.PropertyTypes.StringProperty;
-    relationshipEntity: ComponentFramework.PropertyTypes.StringProperty;
-    linkedEntityFetchXmlResource: ComponentFramework.PropertyTypes.StringProperty;
->>>>>>> a16108c7a19f0922aa28c4f5eb2d2797efc12caa
     NewRecordCreation: ComponentFramework.PropertyTypes.EnumProperty<"1" | "0">;
 }
 export interface IOutputs {
